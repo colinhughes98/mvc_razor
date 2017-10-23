602e5eb4eb1cfa0b9224453ae10cf65b72468660
colinhughes98 <colinhughes98@gmail.com>
2017-10-23 12:47:23 +0100

    first commit

---
+++ MVC_Razor/Models/PersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Razor.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        [DisplayName("Previous roles")]
+        public IList<string> PreviousJobs { get; set; }
+    }
+}
